808e6fbf2a172e20dd8372f89857fcb591ec9352
ItsAshu-2407 <ashutosh24official@gmail.com>
2022-07-23 13:24:11 +0530

    commit

--- 20-07-22/getprime.java
+++ 20-07-22/getprime.java
@@ -1,24 +1,26 @@
 import java.util.*;
-class findPrime
+class primeNumber
 {
  public static void main(String args[])
  {
   int getnum,i,temp=0;
   Scanner sc = new Scanner(System.in);
+  System.out.println("Enter the number to check whether it is prime or not? ");
   getnum = sc.nextInt();
-   for(i=1;i<=getnum;i++)
-   {
-	for(i=2;i<=getnum;i++)
+	for(i=2;i<getnum;i++)
 	{
 		if(getnum%i==0)
   		{
-        		 temp+=1;			
+        		 temp = temp+1;			
  		}
 	}
 	if(temp==0)
  	{
-		System.out.println(getnum+" is a prime number.");
+		System.out.println(getnum+" is a PRIME number.");
 	}   
-   }
+	else
+	{
+		System.out.println(getnum+" is NOT a prime number.");
+	}
  }
 }
